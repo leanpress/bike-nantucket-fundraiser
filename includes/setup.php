efb1348382ca14f7b278f83f356794f805c18997
Au Coeur Design <amber@aucoeurdesign.com>
2015-02-17 11:54:05 -0700

    Add team taxonomy to campaigns

--- includes/setup.php
+++ includes/setup.php
@@ -450,4 +450,42 @@ function _bnfund_register_types() {
 	register_post_type( 'bnfund_campaign_list' );
 }
 
+// hook into the init action and call create_campaign_team_taxonomies when it fires
+add_action( 'init', 'create_campaign_team_taxonomies', 0 );
+
+// create team taxonomy for the post type "campaign"
+function create_campaign_team_taxonomies() {
+		// Add new taxonomy, NOT hierarchical (like tags)
+	$labels = array(
+		'name'                       => _x( 'Teams', 'taxonomy general name' ),
+		'singular_name'              => _x( 'Team', 'taxonomy singular name' ),
+		'search_items'               => __( 'Search Teams' ),
+		'popular_items'              => __( 'Popular Teams' ),
+		'all_items'                  => __( 'All Teams' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Team' ),
+		'update_item'                => __( 'Update Team' ),
+		'add_new_item'               => __( 'Add New Team' ),
+		'new_item_name'              => __( 'New Team Name' ),
+		'separate_items_with_commas' => __( 'Separate teams with commas' ),
+		'add_or_remove_items'        => __( 'Add or remove teams' ),
+		'choose_from_most_used'      => __( 'Choose from the most used teams' ),
+		'not_found'                  => __( 'No teams found.' ),
+		'menu_name'                  => __( 'Teams' ),
+	);
+
+	$args = array(
+		'hierarchical'          => false,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'team' ),
+	);
+
+	register_taxonomy( 'team', 'bnfund_campaign', $args );
+}
+
 ?>
